4298b2f328b07bde96f5366edf85c939a2bdd551
Yusef Habib <habibfernandez@gmail.com>
2017-06-17 14:14:29 +0200

    Webpack bug with entry point .ts

--- angular/src/app.js
+++ angular/src/app.js
@@ -1,6 +1,4 @@
 import greeting from './module1';
 import angular from 'angular';
-
 console.log(greeting);
 console.log(angular);
-
--- angular/webpack.config.js
+++ angular/webpack.config.js
@@ -1,59 +1,69 @@
-var path = require("path");
-
-var config = {
+var path = require('path');
+module.exports = {
+  
   /*
-   * app.ts represents the entry point to the application. Webpack will
-   * recursively go through every "require" statement in app.ts and
-   * efficiently build out the application's dependency tree.
-   */
-  entry:  __dirname + '/src/app.ts',
+  * app.ts represents the entry point to the application. Webpack will
+  * recursively go through every "require" statement in app.ts and
+  * efficiently build out the application's dependency tree.
+  */
+  entry:  __dirname + '/src/app.js',
 
   /*
-   * The combination of path and filename tells Webpack what name to give to
-   * the final bundled JavaScript file and where to store this file.
-   * Public path is fo webpack-dev-server
-   */
+  * The combination of path and filename tells Webpack what name to give to
+  * the final bundled JavaScript file and where to store this file.
+  * Public path is fo webpack-dev-server
+  */
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/'
   },
-
+  
   /*
-   * Lets Webpack now in advance what file extensions you plan on
-   * "require"ing into the web application, and allows you to drop them
-   * in your code.
-   */
+  * Lets Webpack now in advance what file extensions you plan on
+  * "require"ing into the web application, and allows you to drop them
+  * in your code.
+  */
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".js", ".ts"]
   },
-
   module: {
+    
     /*
-     * Each loader needs an associated Regex test that goes through each
-     * of the files you've included (or in this case, all files but the
-     * ones in the excluded directories) and finds all files that pass
-     * the test. Then it will apply the loader to that file. Starting with
-     * the last on in the array
-     */
+    * Each loader needs an associated Regex test that goes through each
+    * of the files you've included (or in this case, all files but the
+    * ones in the excluded directories) and finds all files that pass
+    * the test. Then it will apply the loader to that file. Starting with
+    * the last on in the array
+    */
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: ['babel-loader', 'ts-loader'],
+        test: /\.js$/,
         exclude: /node_modules/,
+        loader: 'babel-loader',
       },
       {
-        test: /\.css$/,
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      { 
+        test: /\.html$/, 
+        exclude: /(node_modules)/,
+        loader: "html" 
+      },
+      { 
+        test: /\.css$/, 
         exclude: /(node_modules)/,
         loaders: ['style-loader', 'css-loader']
       },
       {
-        test: /\.jpg/,
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
         exclude: /(node_modules)/,
         loader: 'url-loader'
-      }
-    ]
-  }
-};
+      },
 
-module.exports = config;
+    ]
+  },
+  devtool: "#inline-source-map"
+}
